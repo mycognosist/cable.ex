a7eff1b561183ff94e8264c5c1ee19d2ead76fca
mycognosist <gnomad@cryptolab.net>
2023-09-16 11:50:06 +0200

    export encode and decode methods to package root

--- test/cable_ex_test.exs
+++ test/cable_ex_test.exs
@@ -13,13 +13,15 @@ defmodule CableTest do
   @encoded_text_post "25b272a71555322d40efe449a7f99af8fd364b92d350f1664481b2da340a02d06725733046b35fa3a7e8dc0099a2b3dff10d3fd8b0f6da70d094352e3f5d27a8bc3f5586cf0bf71befc22536c3c50ec7b1d64398d43c3f4cde778e579e88af05015049d089a650aa896cb25ec35258653be4df196b4a5e5b6db7ed024aaa89e1b300500764656661756c740d68e282ac6c6c6f20776f726c64"
 
   setup_all do
+    alias Cable.Post
+
     public_key = Base.decode16!(@public_key, case: :lower)
     hash = Base.decode16!(@hash, case: :lower)
     expected_encoding = Base.decode16!(@encoded_text_post, case: :lower)
     secret_key = Base.decode16!(@secret_key, case: :lower)
     signature = Base.decode16!(@signature, case: :lower)
 
-    post = Cable.Post.new_text_post(public_key, [hash], @timestamp, @channel, @text)
+    post = Post.new_text_post(public_key, [hash], @timestamp, @channel, @text)
 
     {:ok,
      text_post: post,
@@ -29,11 +31,11 @@ defmodule CableTest do
   end
 
   test "encodes and signs a text post", state do
-    assert Cable.Encoder.encode(state[:text_post], state[:secret_key]) == state[:expected_encoding]
+    assert Cable.encode(state[:text_post], state[:secret_key]) == state[:expected_encoding]
   end
 
   test "decodes a text post", state do
     signed_post = %{state[:text_post] | signature: state[:signature]}
-    assert Cable.Decoder.decode(state[:expected_encoding]) == signed_post
+    assert Cable.decode(state[:expected_encoding]) == signed_post
   end
 end
